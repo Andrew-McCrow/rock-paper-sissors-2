34e97af12999e849c96208aa3a3f8eb9b5c1ea67
Andrew-McCrow <andrewmccrow@gmail.com>
2025-11-14 20:39:33 +0000

    Added human choice

--- rps.js
+++ rps.js
@@ -1,9 +1,19 @@
-console.log("Hello World")
-
 function getComputerChoice() {
     const choices = ['rock', 'paper', 'scissors'];  
     const randomIndex = Math.floor(Math.random() * choices.length);
     return choices[randomIndex];      
 }
+
+function getHumanChoice()
+{
+    let input = prompt("Do you choose rock, paper or scissors?");
+    input = input.toLowerCase();
+    if (input === 'rock' || input === 'paper' || input === 'scissors') {
+        return input;
+    } else {
+        console.log("Invalid choice. Please choose rock, paper, or scissors.");
+        return getHumanChoice();
+    }
+}   
 
 console.log(getComputerChoice());
